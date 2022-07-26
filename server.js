28dd292ba5b0dadc4014c720a4c5307653f62298
Gonzo Nieto <gonzo.nieto@shopify.com>
2022-07-26 15:47:46 -0400

    The rest of the owl

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
+// Import Express into this file
 const express = require('express');
-const app = express();
-const port = 3000;
 
-app.length('/', (req, res) => {
-  res.send('Hello World!');
+// https://nodejs.org/api/path.html
+const path = require('path');
+
+require('dotenv').config();
+
+///////////////
+// CONFIGURATION
+
+// Create an instance of an Express app
+const app = express();
+
+// Set EJS as view engine
+app.set("view engine", "ejs");
+
+// Define what port the app will run on
+const port = process.env.PORT || 8080;
+
+// Serve files in /public statically
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Define views folder
+app.set('views', path.join(__dirname, 'views'));
+///////////////
+
+///////////////
+// ROUTING
+// GET requests on root
+app.get('/', (req, res) => {
+  res.render('index');
 });
+
+// GET requests on /projects
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+// GET on any undefined routes
+app.get('/*', (req, res) => {
+  res.redirect('/');
+});
+///////////////
 
 app.listen(port, () => {
   console.log(`Express learning server listening on port ${port}`);
